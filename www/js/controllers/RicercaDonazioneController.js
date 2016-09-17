22e0599a03794ed1efcc048af6eee14d07a2c8ac
SMASCARE <SMASCARE@LIT000198.corp.capgemini.com>
2016-09-17 15:46:44 +0200

    Ricerca donazione - crea carrello

--- www/js/controllers/RicercaDonazioneController.js
+++ www/js/controllers/RicercaDonazioneController.js
@@ -2,19 +2,109 @@
  * Created by Aerdna on 17-Sep-16.
  */
 myApp.controller('ricercaDonazioneController', function($scope,$http) {
+	
     $scope.chiudiTipo=function(){
 
-        $scope.goOpzioniClassificazione = false;
+
+       // $scope.goOpzioniClassificazione = false;
         $scope.goOpzioniTipo = ! $scope.goOpzioniTipo;
-        console.log("TIPO: "+$scope.goOpzioniTipo);
-        console.log("CLASS: "+$scope.goOpzioniClassificazione);
+        
+//        console.log("CLASS: "+$scope.goOpzioniClassificazione);
+//        console.log("TIPO: "+$scope.goOpzioniTipo);
 
         if($scope.goOpzioniTipo)  {
             $scope.goOpzioniClassificazione = false;
-            $scope.goOpzioniAllergeni = false;
+            $scope.goOpzioniAllergene = false;
             $scope.goOpzioniProdotto = false;
             $scope.goOpzioniCategoria = false;
             $scope.goOpzioniSottocategoria = false;
+        }
+
+    }
+    
+    
+    $scope.chiudiClassificazione=function(){
+
+        //$scope.goOpzioniClassificazione = false;
+        $scope.goOpzioniClassificazione = ! $scope.goOpzioniClassificazione;
+        
+//        console.log("CLASS: "+$scope.goOpzioniClassificazione);
+//        console.log("Classificazione: "+$scope.goOpzioniClassificazione);
+
+        if($scope.goOpzioniClassificazione)  {
+            $scope.goOpzioniTipo = false;
+            $scope.goOpzioniAllergene = false;
+            $scope.goOpzioniProdotto = false;
+            $scope.goOpzioniCategoria = false;
+            $scope.goOpzioniSottocategoria = false;
+        }
+    }
+    
+    $scope.chiudiAllergene=function(){
+
+        //$scope.goOpzioniClassificazione = false;
+        $scope.goOpzioniAllergene = ! $scope.goOpzioniAllergene;
+        
+//        console.log("CLASS: "+$scope.goOpzioniClassificazione);
+//        console.log("Classificazione: "+$scope.goOpzioniClassificazione);
+
+        if($scope.goOpzioniAllergene)  {
+            $scope.goOpzioniTipo = false;
+            $scope.goOpzioniClassificazione = false;
+            $scope.goOpzioniProdotto = false;
+            $scope.goOpzioniCategoria = false;
+            $scope.goOpzioniSottocategoria = false;
+        }
+    }
+    
+    $scope.chiudiProdotto=function(){
+
+        //$scope.goOpzioniClassificazione = false;
+        $scope.goOpzioniProdotto = ! $scope.goOpzioniProdotto;
+        
+//        console.log("CLASS: "+$scope.goOpzioniClassificazione);
+//        console.log("Classificazione: "+$scope.goOpzioniClassificazione);
+
+        if($scope.goOpzioniProdotto)  {
+            $scope.goOpzioniTipo = false;
+            $scope.goOpzioniClassificazione = false;
+            $scope.goOpzioniAllergene = false;
+            $scope.goOpzioniCategoria = false;
+            $scope.goOpzioniSottocategoria = false;
+        }
+    }
+
+    $scope.chiudiCategoria=function(){
+
+        //$scope.goOpzioniClassificazione = false;
+        $scope.goOpzioniCategoria = ! $scope.goOpzioniCategoria;
+        
+//        console.log("CLASS: "+$scope.goOpzioniClassificazione);
+//        console.log("Classificazione: "+$scope.goOpzioniClassificazione);
+
+        if($scope.goOpzioniCategoria)  {
+            $scope.goOpzioniTipo = false;
+            $scope.goOpzioniClassificazione = false;
+            $scope.goOpzioniAllergene = false;
+            $scope.goOpzioniProdotto = false;
+            $scope.goOpzioniSottocategoria = false;
+        }
+    }
+    
+    $scope.chiudiSottocategoria=function(){
+
+        //$scope.goOpzioniClassificazione = false;
+        $scope.goOpzioniSottocategoria = ! $scope.goOpzioniSottocategoria;
+        
+//        console.log("CLASS: "+$scope.goOpzioniClassificazione);
+//        console.log("Classificazione: "+$scope.goOpzioniClassificazione);
+
+        if($scope.goOpzioniSottocategoria)  {
+            $scope.goOpzioniTipo = false;
+            $scope.goOpzioniClassificazione = false;
+            $scope.goOpzioniAllergene = false;
+            $scope.goOpzioniCategoria = false;
+            $scope.goOpzioniProdotto = false;
         }
     }
 
@@ -23,16 +113,18 @@ myApp.controller('ricercaDonazioneController', function($scope,$http) {
     $scope.load = function(page) {
         $scope.mySplitterContent.load(page)
     }
+    
     $scope.open = function() {
         $scope.mySplitterSide.open();
     }
+    
     $scope.groups = [];
-    for (var i = 0; i < 5; i++) {
+    for (var i = 0; i < 1; i++) { //5
         $scope.groups[i] = {
             name: i,
             items: []
         };
-        for (var j = 0; j < 3; j++) {
+        for (var j = 0; j < 3; j++) { 
             $scope.groups[i].items.push(i + '-' + j);
         }
     }
@@ -48,6 +140,7 @@ myApp.controller('ricercaDonazioneController', function($scope,$http) {
             $scope.shownGroup = group;
         }
     };
+    
     $scope.toggleoptions = function(group) {
         if ($scope.isGroupShown(group)) {
             $scope.shownGroup = null;
