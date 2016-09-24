f3e5ae103d1a4f971da0fdbcb0aacf7b48b44232
SMASCARE <SMASCARE@LIT000198.corp.capgemini.com>
2016-09-24 12:45:36 +0200

    Aggiornate/create classi:
    _registration.html
    additem.html 
    conferma.html 
    dettaglioProdotto.html
    home
    Hpsede
    indexG
    indexV 
    inserimentoNuovoProdotto.html
    itemdetails.html
    memo.html
    ModificaDonazione.html
    RegistrationController.js
    AddItemController.js
    ConfermaController.js
    DettaglioProdottoController.js
    no-controller
    no-controller
    indexG-old
    indexV-old
    inserimentoNuovoProdottoController.js
    itemdetails.html-old
    no-controller
    ModificaDonazioneController.js

---
+++ www/js/controllers/ConfermaController.js
@@ -0,0 +1,40 @@
+
+myApp.controller('confermaController', function($scope,$http) { 
+	
+
+//		 $scope.ready(function() {
+//             console.log("Onsen UI is ready!");
+//             /*var query2="https://joyfoodamministratore-sisp.rhcloud.com/detailProduct?id=$";*/
+//             var query2="https://joyfoodamministratore-sisp.rhcloud.com/detailProduct?id=1";       
+//             console.log(query2);
+//             var jqxhr = $.ajax( query2 )
+//                 .done(function(data) {
+//                     var obj=JSON.parse(data);
+//                     var id = obj.id;
+//                     var nome = obj.nome;
+//                     var descrizione = obj.descrizione;
+//                     var descrizioneEstesa = obj.descrizioneEstesa;
+//                     var sottocategoria = obj.sottocategoria;
+//                     var classificazione = obj.classificazione;
+//                     var tipologia = obj.tipologia;
+//                     var allergene = obj.allergene;
+//                     var datacreazione = obj.datacreazione;
+//                     document.getElementById('id').innerHTML = id;
+//                     document.getElementById('nome').innerHTML = nome;
+//                     document.getElementById('descrizione').innerHTML = descrizione;
+//                     document.getElementById('descrizioneEstesa').innerHTML = descrizioneEstesa;
+//                     document.getElementById('sottocategoria').innerHTML = sottocategoria;
+//                     document.getElementById('classificazione').innerHTML = classificazione;
+//                     document.getElementById('tipologia').innerHTML = tipologia;
+//                     document.getElementById('allergene').innerHTML = allergene;
+//                     document.getElementById('datacreazione').innerHTML = datacreazione;
+//                 }
+       
+             
+         
+             
+ });         
+		
+    
+    
+ 
