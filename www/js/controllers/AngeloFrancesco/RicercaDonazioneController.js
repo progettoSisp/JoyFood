0a55d2f1b99f23c4dae6223f0e7904855b09ff25
Aerdna <Aerdna@Aerdna-PC>
2016-10-08 12:23:17 +0200

    Update

--- www/js/controllers/AngeloFrancesco/RicercaDonazioneController.js
+++ www/js/controllers/AngeloFrancesco/RicercaDonazioneController.js
@@ -1,155 +1,66 @@
-/**
- * Created by Aerdna on 17-Sep-16.
- */
 myApp.controller('ricercaDonazioneController', function($scope,$http) {
-	
-    $scope.chiudiTipo=function(){
-
-    	 // $scope.goOpzioniClassificazione = false;
-        $scope.goOpzioniTipo = ! $scope.goOpzioniTipo;
+	$scope.opzioni="Più Opzioni";
+    $scope.goOpzioni = false;
+         
+   $scope.cambio=function(){
         
-//        console.log("CLASS: "+$scope.goOpzioniClassificazione);
-//        console.log("TIPO: "+$scope.goOpzioniTipo);
-
-        if($scope.goOpzioniTipo)  {
-            $scope.goOpzioniClassificazione = false;
-            $scope.goOpzioniAllergene = false;
-            $scope.goOpzioniProdotto = false;
-            $scope.goOpzioniCategoria = false;
-            $scope.goOpzioniSottocategoria = false;
-        }
-
+             
+             $scope.goOpzioni = ! $scope.goOpzioni;
+             if( $scope.goOpzioni){
+                  $scope.opzioni="Meno Opzioni";
+             }else{
+                   $scope.opzioni="Più Opzioni";  
+             }
     }
+   
+   $scope.load = function(page) {
+     $scope.mySplitterContent.load(page)
+   }
+   $scope.open = function() {
+     $scope.mySplitterSide.open();
+   }
+   
+    
+    $scope.accordionPrimoLivello = ['Tipo','Classificazione','Allergeni'];
+
+    
+    $scope.accordionSecondoLivello = [['Carne','Pesce','Frutta e verdura','Grano e cereali','Latticini'],['Vegano','Vegetariano','Celiaco','Non Specificato'],['Mais','Latteria','Uovo','Pesce','Aromi','Glutine','MSG','Arachidi','Solanaceae','Molluschi','Soia','Solfiti','Grassi Trans','Frutta a guscio','Grano']];
     
     
-    $scope.chiudiClassificazione=function(){
+  $scope.groups = [];
+for (var i = 0; i < $scope.accordionPrimoLivello.length; i++) {
+$scope.groups[i] = {
+name: $scope.accordionPrimoLivello[i],
+items: []
+};
+for (var j = 0; j < $scope.accordionSecondoLivello[i].length; j++) {
+$scope.groups[i].items.push($scope.accordionSecondoLivello[i][j]);
 
-        //$scope.goOpzioniClassificazione = false;
-        $scope.goOpzioniClassificazione = ! $scope.goOpzioniClassificazione;
-        
-//        console.log("CLASS: "+$scope.goOpzioniClassificazione);
-//        console.log("Classificazione: "+$scope.goOpzioniClassificazione);
+}
+}
 
-        if($scope.goOpzioniClassificazione)  {
-            $scope.goOpzioniTipo = false;
-            $scope.goOpzioniAllergene = false;
-            $scope.goOpzioniProdotto = false;
-            $scope.goOpzioniCategoria = false;
-            $scope.goOpzioniSottocategoria = false;
-        }
-    }
-    
-    $scope.chiudiAllergene=function(){
+/*
+* if given group is the selected group, deselect it
+* else, select the given group
+*/
+$scope.toggleGroup = function(group) {
+if ($scope.isGroupShown(group)) {
+$scope.shownGroup = null;
+} else {
+$scope.shownGroup = group;
+}
+};
+$scope.toggleoptions = function(group) {
+if ($scope.isGroupShown(group)) {
+$scope.shownGroup = null;
+} else {
+$scope.shownGroup = group;
+}
+};
 
-        //$scope.goOpzioniClassificazione = false;
-        $scope.goOpzioniAllergene = ! $scope.goOpzioniAllergene;
-        
-//        console.log("CLASS: "+$scope.goOpzioniClassificazione);
-//        console.log("Classificazione: "+$scope.goOpzioniClassificazione);
-
-        if($scope.goOpzioniAllergene)  {
-            $scope.goOpzioniTipo = false;
-            $scope.goOpzioniClassificazione = false;
-            $scope.goOpzioniProdotto = false;
-            $scope.goOpzioniCategoria = false;
-            $scope.goOpzioniSottocategoria = false;
-        }
-    }
-    
-    $scope.chiudiProdotto=function(){
-
-        //$scope.goOpzioniClassificazione = false;
-        $scope.goOpzioniProdotto = ! $scope.goOpzioniProdotto;
-        
-//        console.log("CLASS: "+$scope.goOpzioniClassificazione);
-//        console.log("Classificazione: "+$scope.goOpzioniClassificazione);
-
-        if($scope.goOpzioniProdotto)  {
-            $scope.goOpzioniTipo = false;
-            $scope.goOpzioniClassificazione = false;
-            $scope.goOpzioniAllergene = false;
-            $scope.goOpzioniCategoria = false;
-            $scope.goOpzioniSottocategoria = false;
-        }
-    }
-    
-    $scope.chiudiCategoria=function(){
-
-        //$scope.goOpzioniClassificazione = false;
-        $scope.goOpzioniCategoria = ! $scope.goOpzioniCategoria;
-        
-//        console.log("CLASS: "+$scope.goOpzioniClassificazione);
-//        console.log("Classificazione: "+$scope.goOpzioniClassificazione);
-
-        if($scope.goOpzioniCategoria)  {
-            $scope.goOpzioniTipo = false;
-            $scope.goOpzioniClassificazione = false;
-            $scope.goOpzioniAllergene = false;
-            $scope.goOpzioniProdotto = false;
-            $scope.goOpzioniSottocategoria = false;
-        }
-    }
-    
-    $scope.chiudiSottocategoria=function(){
-
-        //$scope.goOpzioniClassificazione = false;
-        $scope.goOpzioniSottocategoria = ! $scope.goOpzioniSottocategoria;
-        
-//        console.log("CLASS: "+$scope.goOpzioniClassificazione);
-//        console.log("Classificazione: "+$scope.goOpzioniClassificazione);
-
-        if($scope.goOpzioniSottocategoria)  {
-            $scope.goOpzioniTipo = false;
-            $scope.goOpzioniClassificazione = false;
-            $scope.goOpzioniAllergene = false;
-            $scope.goOpzioniCategoria = false;
-            $scope.goOpzioniProdotto = false;
-        }
-    }
-
-
-
-    $scope.load = function(page) {
-        $scope.mySplitterContent.load(page)
-    }
-    
-    $scope.open = function() {
-        $scope.mySplitterSide.open();
-    }
-    
-    $scope.groups = [];
-    for (var i = 0; i < 1; i++) { //5
-        $scope.groups[i] = {
-            name: i,
-            items: []
-        };
-        for (var j = 0; j < 3; j++) { 
-            $scope.groups[i].items.push(i + '-' + j);
-        }
-    }
-
-    /*
-     * if given group is the selected group, deselect it
-     * else, select the given group
-     */
-    $scope.toggleGroup = function(group) {
-        if ($scope.isGroupShown(group)) {
-            $scope.shownGroup = null;
-        } else {
-            $scope.shownGroup = group;
-        }
-    };
-    
-    $scope.toggleoptions = function(group) {
-        if ($scope.isGroupShown(group)) {
-            $scope.shownGroup = null;
-        } else {
-            $scope.shownGroup = group;
-        }
-    };
-
-    $scope.isGroupShown = function(group) {
-        return $scope.shownGroup === group;
-    };
+$scope.isGroupShown = function(group) {
+return $scope.shownGroup === group;
+};
+ 
 
 });
