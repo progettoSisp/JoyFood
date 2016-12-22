2779f76e5f57ad72102013c208ee467b60293c04
Angaroni <Angaroni.andrea@gmail.com>
2016-12-22 18:13:11 +0100

    Update

--- www/js/controllers/AngeloFrancesco/risultatiRicercaController.js
+++ www/js/controllers/AngeloFrancesco/risultatiRicercaController.js
@@ -1,7 +1,7 @@
 myApp.controller('risultatiRicercaController', function($scope,$http, ricercaService,donazioniService) {
 	
   $scope.carrelli=[];
-  
+  $scope.date=new Date().getTime();
   $scope.init=function(){
 	  $scope.carrelli =ricercaService.getRicerca();
 	  console.log($scope.carrelli);
